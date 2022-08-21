cf227d39305ee8e60d14611c62264a457841db21
mahmoudabdalkarim94 <m.abdalkareem@foothillsolutions.com>
2022-08-22 01:57:44 +0300

    React Testing Crash Course - Add React Testing Library Material

---
+++ react-testing-library/src/App.tsx
@@ -0,0 +1,11 @@
+import Checkout from "./components/Checkout";
+
+function App() {
+    return (
+        <div className="App">
+            <Checkout/>
+        </div>
+    );
+}
+
+export default App;
